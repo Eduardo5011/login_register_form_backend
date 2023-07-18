213c280c718410d7a4f3315814323d3465e3a7b8
Eduardo5011 <eduardo.rosado5011@gmail.com>
2023-07-18 17:56:04 -0400

    updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,5 @@
-const allowedOrigins = ["https://login-register-101.onrender.com"];
+const allowedOrigins = [
+    "https://login-register-101-backend.onrender.com"
+];
 
 module.exports = allowedOrigins;
